b92eee5a5bd60144cb53f566dfe523d588823b70
Laurenz Werner <werner.la@protonmail.com>
2023-06-22 23:05:44 +0200

    add initCrypto() to LibraryConnector

--- app/src/main/java/dawn/android/LibraryConnector.kt
+++ app/src/main/java/dawn/android/LibraryConnector.kt
@@ -6,10 +6,18 @@ import kotlinx.serialization.json.Json
 @Serializable
 data class TempId(val status: String, val id: String? = null)
 
+@Serializable
+data class InitCrypto(val status: String, val id: String? = null)
+
 object LibraryConnector {
 
     init {
         System.loadLibrary("dawn")
+    }
+
+    fun mInitCrypto(): InitCrypto {
+        val libraryResponseJSON = initCrypto()
+        return Json.decodeFromString(libraryResponseJSON)
     }
 
     fun mGetTempId(id: String): TempId {
@@ -17,5 +25,6 @@ object LibraryConnector {
         return Json.decodeFromString(libraryResponseJSON)
     }
 
+    private external fun initCrypto(): String
     private external fun getTempId(id: String): String
 }
