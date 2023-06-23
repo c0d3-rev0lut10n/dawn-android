26268792b1a873a409a76241b7ce33c39445dfd5
Laurenz Werner <werner.la@protonmail.com>
2023-06-23 11:41:41 +0200

    provide interface to signKeygen()

--- app/src/main/java/dawn/android/LibraryConnector.kt
+++ app/src/main/java/dawn/android/LibraryConnector.kt
@@ -7,6 +7,9 @@ import kotlinx.serialization.json.Json
 data class InitCrypto(val status: String, val id: String? = null)
 
 @Serializable
+data class SignKeys(val status: String, val own_pubkey_sig: String? = null, val own_seckey_sig: String? = null)
+
+@Serializable
 data class TempId(val status: String, val id: String? = null)
 
 object LibraryConnector {
@@ -20,11 +23,17 @@ object LibraryConnector {
         return Json.decodeFromString(libraryResponseJSON)
     }
 
+    fun mSignKeygen(): SignKeys {
+        val libraryResponseJSON = signKeygen()
+        return Json.decodeFromString(libraryResponseJSON)
+    }
+
     fun mGetTempId(id: String): TempId {
         val libraryResponseJSON = getTempId(id)
         return Json.decodeFromString(libraryResponseJSON)
     }
 
     private external fun initCrypto(): String
+    private external fun signKeygen(): String
     private external fun getTempId(id: String): String
 }
