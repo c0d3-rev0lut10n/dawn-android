c1951c4180cd3367c1f81b87d1ebee9fd4d76372
Laurenz Werner <werner.la@protonmail.com>
2023-06-30 23:27:00 +0200

    start work on SettingsActivity

--- app/src/main/java/dawn/android/SettingsActivity.kt
+++ app/src/main/java/dawn/android/SettingsActivity.kt
@@ -1,11 +1,48 @@
 package dawn.android
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import androidx.appcompat.app.AppCompatActivity
+import dawn.android.data.Theme
+import dawn.android.databinding.ActivitySettingsBinding
+import dawn.android.util.ThemeLoader
 
 class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySettingsBinding
+    private lateinit var mTheme: Theme
+    private lateinit var actionBarText: SpannableString
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
+
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val mThemeLoader = ThemeLoader(this)
+        mTheme = mThemeLoader.loadDarkTheme()
+
+        window.statusBarColor = mTheme.primaryBackgroundColor
+
+        val actionBar = binding.toolbar
+        val actionBarTextColor = mTheme.primaryTextColor
+        val actionBarString = "Settings"
+        actionBarText = SpannableString(actionBarString)
+        actionBarText.setSpan(ForegroundColorSpan(actionBarTextColor), 0, actionBarString.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        actionBar.setBackgroundColor(mTheme.primaryBackgroundColor)
+
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(mTheme.backButtonIcon)
+    }
+
+    override fun onResume() {
+        binding.toolbar.title = actionBarText
+        super.onResume()
     }
 }
