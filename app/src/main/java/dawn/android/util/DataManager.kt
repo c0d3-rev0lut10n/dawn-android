cbf8130b94463d38e4fda20adf38ed36bd43ecf7
Laurenz Werner <werner.la@protonmail.com>
2023-08-04 01:00:16 +0200

    fix padding length

--- app/src/main/java/dawn/android/util/DataManager.kt
+++ app/src/main/java/dawn/android/util/DataManager.kt
@@ -157,7 +157,7 @@ object DataManager {
         val paddingLength = SecureRandom().nextInt(150) + 50
         val padding = CharArray(paddingLength)
         val stringGenerationRng = SecureRandom()
-        for(i in 0 until paddingLength-1) {
+        for(i in 0 until paddingLength) {
             padding[i] = availablePaddingCharacters[stringGenerationRng.nextInt(availablePaddingCharacters.length)]
         }
         return padding
