b39c38d273e462d02840e22e79d42d36a3886d46
Laurenz Werner <werner.la@protonmail.com>
2023-08-04 00:02:18 +0200

    make SettingsActivity comply to default theming

--- app/src/main/java/dawn/android/SettingsActivity.kt
+++ app/src/main/java/dawn/android/SettingsActivity.kt
@@ -18,19 +18,27 @@
 
 package dawn.android
 
+import android.os.Build
 import android.os.Bundle
 import android.text.Spannable
 import android.text.SpannableString
 import android.text.style.ForegroundColorSpan
+import android.view.WindowInsets
+import android.view.WindowInsetsController
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import dawn.android.data.Preferences
 import dawn.android.data.Theme
 import dawn.android.databinding.ActivitySettingsBinding
+import dawn.android.databinding.ActivitySetupBinding
 import dawn.android.util.ThemeLoader
 
 class SettingsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySettingsBinding
     private lateinit var mTheme: Theme
+    private var androidTheme: Int = 0
     private lateinit var actionBarText: SpannableString
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,17 +50,38 @@ class SettingsActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
 
         val mThemeLoader = ThemeLoader(this)
-        mTheme = mThemeLoader.loadDarkTheme()
+        val themeSwitch = mThemeLoader.getThemeSetting(this)
+        when(themeSwitch) {
+            Preferences.THEME_DARK -> {
+                theme.applyStyle(R.style.Theme_Dawn_Dark, true)
+                mTheme = mThemeLoader.loadDarkTheme()
+                androidTheme = R.style.Theme_Dawn_Dark
+            }
 
-        window.statusBarColor = mTheme.primaryBackgroundColor
+            Preferences.THEME_EXTRADARK -> {
+                theme.applyStyle(R.style.Theme_Dawn_ExtraDark, true)
+                mTheme = mThemeLoader.loadExtraDarkTheme()
+                androidTheme = R.style.Theme_Dawn_ExtraDark
+                // hide status bar and navigation bar
+                if(Build.VERSION.SDK_INT < 30) {
+                    // effect may not work on even older API levels
+                    WindowInsetsControllerCompat(window, window.decorView).hide(WindowInsetsCompat.Type.statusBars() or WindowInsetsCompat.Type.navigationBars())
+                    WindowInsetsControllerCompat(window, window.decorView).systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+                }
+                else {
+                    window.decorView.windowInsetsController?.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+                    window.decorView.windowInsetsController?.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+                }
+            }
+        }
 
-        val actionBar = binding.toolbar
+        window.statusBarColor = mTheme.primaryUIColor
+        window.navigationBarColor = mTheme.primaryBackgroundColor
+
         val actionBarTextColor = mTheme.primaryTextColor
         val actionBarString = getString(R.string.settings)
         actionBarText = SpannableString(actionBarString)
         actionBarText.setSpan(ForegroundColorSpan(actionBarTextColor), 0, actionBarString.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-
-        actionBar.setBackgroundColor(mTheme.primaryBackgroundColor)
 
         supportActionBar?.setHomeButtonEnabled(true)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
