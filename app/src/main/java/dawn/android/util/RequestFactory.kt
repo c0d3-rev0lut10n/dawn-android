1e85e55ae261cdae53395b9124997e34f00bde0b
Laurenz Werner <werner.la@protonmail.com>
2023-08-13 10:13:56 +0200

    implement detail request

--- app/src/main/java/dawn/android/util/RequestFactory.kt
+++ app/src/main/java/dawn/android/util/RequestFactory.kt
@@ -14,4 +14,9 @@ object RequestFactory {
         val request = Request.Builder().url("$serverBaseAddress/rcv/$id/$messageNumber")
         return request.build()
     }
+
+    fun buildDetailRequest(id: String, messageNumber: UShort, mdc: String): Request {
+        val request = Request.Builder().url("$serverBaseAddress/d/$id/$messageNumber?mdc=$mdc")
+        return request.build()
+    }
 }
