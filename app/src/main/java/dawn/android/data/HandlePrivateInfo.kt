e3e6fdf53ddb467f92abfae3372b2eae91a545e4
Laurenz Werner <werner.la@protonmail.com>
2023-10-16 23:18:58 +0200

    add class for de-/serializing generated init keys

---
+++ app/src/main/java/dawn/android/data/HandlePrivateInfo.kt
@@ -0,0 +1,49 @@
+/*	Copyright (c) 2023 Laurenz Werner
+
+	This file is part of Dawn.
+
+	Dawn is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Dawn is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Dawn.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package dawn.android.data
+
+import dawn.android.CurveKeys
+import dawn.android.KyberKeys
+
+class HandlePrivateInfo (
+    val initKeypairKyber: KyberKeys,
+    val initKeypairCurve: CurveKeys,
+    val initKeypairCurvePfs2: CurveKeys,
+    val initKeypairKyberSalt: KyberKeys,
+    val initKeypairCurveSalt: CurveKeys
+) {
+    override fun toString(): String {
+        return initKeypairKyber.own_pubkey_kyber!! + "\n" +
+                initKeypairKyber.own_seckey_kyber!! + "\n" +
+                initKeypairCurve.own_pubkey_curve!! + "\n" +
+                initKeypairCurve.own_seckey_curve!! + "\n" +
+                initKeypairCurvePfs2.own_pubkey_curve!! + "\n" +
+                initKeypairCurvePfs2.own_seckey_curve!! + "\n" +
+                initKeypairKyberSalt.own_pubkey_kyber!! + "\n" +
+                initKeypairKyberSalt.own_seckey_kyber!! + "\n" +
+                initKeypairCurveSalt.own_pubkey_curve!! + "\n" +
+                initKeypairCurveSalt.own_seckey_curve!!
+    }
+
+    companion object {
+        fun fromString(): HandlePrivateInfo {
+            TODO()
+        }
+    }
+}
