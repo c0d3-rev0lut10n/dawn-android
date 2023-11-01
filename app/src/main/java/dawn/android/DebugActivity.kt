5bfcacabe64dbec02f8700be05b75a10288ac9a5
Laurenz Werner <werner.la@protonmail.com>
2023-11-01 21:42:39 +0100

    create basic layout for DebugActivity

--- app/src/main/java/dawn/android/DebugActivity.kt
+++ app/src/main/java/dawn/android/DebugActivity.kt
@@ -1,11 +1,80 @@
 package dawn.android
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import dawn.android.data.Preferences
+import dawn.android.data.Theme
+import dawn.android.databinding.ActivityDebugBinding
+import dawn.android.databinding.ActivitySettingsBinding
+import dawn.android.util.ThemeLoader
 
 class DebugActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDebugBinding
+    private lateinit var mThemeLoader: ThemeLoader
+    private lateinit var mTheme: Theme
+    private var androidTheme: Int = 0
+    private lateinit var actionBarText: SpannableString
+    private lateinit var logTag: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_debug)
+
+        mThemeLoader = ThemeLoader(this)
+        val themeSwitch = mThemeLoader.getThemeSetting(this)
+        when(themeSwitch) {
+            Preferences.THEME_DARK -> {
+                setTheme(R.style.Theme_Dawn_Dark)
+                mTheme = mThemeLoader.loadDarkTheme()
+                androidTheme = R.style.Theme_Dawn_Dark
+            }
+
+            Preferences.THEME_EXTRADARK -> {
+                setTheme(R.style.Theme_Dawn_ExtraDark)
+                mTheme = mThemeLoader.loadExtraDarkTheme()
+                androidTheme = R.style.Theme_Dawn_ExtraDark
+                // hide status bar and navigation bar
+                if(Build.VERSION.SDK_INT < 30) {
+                    // effect may not work on even older API levels
+                    WindowInsetsControllerCompat(window, window.decorView).hide(WindowInsetsCompat.Type.statusBars() or WindowInsetsCompat.Type.navigationBars())
+                    WindowInsetsControllerCompat(window, window.decorView).systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+                }
+                else {
+                    window.decorView.windowInsetsController?.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+                    window.decorView.windowInsetsController?.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+                }
+            }
+        }
+
+        logTag = "$packageName.DebugActivity"
+        binding = ActivityDebugBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        window.statusBarColor = mTheme.primaryUIColor
+        window.navigationBarColor = mTheme.primaryBackgroundColor
+
+        val actionBarTextColor = mTheme.secondaryTextColor
+        val actionBarString = getString(R.string.debug_app_bar)
+        actionBarText = SpannableString(actionBarString)
+        actionBarText.setSpan(ForegroundColorSpan(actionBarTextColor), 0, actionBarString.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(mTheme.backButtonIcon)
+    }
+
+    override fun onResume() {
+        binding.toolbar.title = actionBarText
+        super.onResume()
     }
 }
