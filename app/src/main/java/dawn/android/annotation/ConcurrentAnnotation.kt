4dd19198a1ec2966cf67d895769da514aa25de73
Laurenz Werner <werner.la@protonmail.com>
2024-01-23 21:41:47 +0100

    add ConcurrentAnnotation

---
+++ app/src/main/java/dawn/android/annotation/ConcurrentAnnotation.kt
@@ -0,0 +1,31 @@
+/*	Copyright (c) 2024 Laurenz Werner
+
+	This file is part of Dawn.
+
+	Dawn is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Dawn is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Dawn.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package dawn.android.annotation
+
+@RequiresOptIn(
+    level = RequiresOptIn.Level.ERROR,
+    message = "Be careful with this as you might introduce race conditions!"
+)
+@Retention(AnnotationRetention.BINARY)
+@Target(
+    AnnotationTarget.CLASS,
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.PROPERTY
+)
+annotation class ConcurrentAnnotation
