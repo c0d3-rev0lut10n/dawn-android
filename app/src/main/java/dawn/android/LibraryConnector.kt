52cd967af35d06799be49b3e131f319db6214b02
Laurenz Werner <werner.la@protonmail.com>
2024-03-09 11:28:31 +0100

    allow for UInt timestamps

--- app/src/main/java/dawn/android/LibraryConnector.kt
+++ app/src/main/java/dawn/android/LibraryConnector.kt
@@ -1,9 +1,28 @@
+/*
+ * Copyright (c) 2024  Laurenz Werner
+ *
+ * This file is part of Dawn.
+ *
+ * Dawn is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Dawn is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Dawn.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package dawn.android
 
 import dawn.android.data.Result
-import dawn.android.data.Result.Companion.ok
 import dawn.android.data.Result.Companion.err
-import kotlinx.serialization.*
+import dawn.android.data.Result.Companion.ok
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 
 @Serializable
@@ -294,6 +313,13 @@ object LibraryConnector {
 
     fun mGetAllTimestampsSince(timestamp: String): Result<MultiTimestamp, String> {
         val libraryResponse: MultiTimestamp = Json.decodeFromString(getAllTimestampsSince(timestamp))
+        if(libraryResponse.status != "ok") return err(libraryResponse.status)
+        return ok(libraryResponse)
+    }
+
+    fun mGetAllTimestampsSince(timestamp: UInt): Result<MultiTimestamp, String> {
+        val convertedTimestamp = timestamp.toString()
+        val libraryResponse: MultiTimestamp = Json.decodeFromString(getAllTimestampsSince(convertedTimestamp))
         if(libraryResponse.status != "ok") return err(libraryResponse.status)
         return ok(libraryResponse)
     }
