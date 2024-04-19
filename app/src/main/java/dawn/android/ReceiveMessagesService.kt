45e4de68d1bc88322ca13b5fbf44e282abdc48dc
Laurenz Werner <werner.la@protonmail.com>
2024-04-19 20:46:36 +0200

    more code for message transmission

--- app/src/main/java/dawn/android/ReceiveMessagesService.kt
+++ app/src/main/java/dawn/android/ReceiveMessagesService.kt
@@ -174,7 +174,31 @@ class ReceiveMessagesService: Service() {
         tickInProgress = false
     }
 
+    @OptIn(ConcurrentAnnotation::class)
     private fun transmitMessages(): Result<Ok, String> {
+        for(task in transmissionQueue) {
+            val chatResult = ChatManager.getChat(task.chatDataID)
+            if(chatResult.isErr()) continue
+            val chat = chatResult.unwrap()
+            val tempId = LibraryConnector.mGetTempId(chat.id).unwrap().id!!
+            val mdc = LibraryConnector.mPredictableMdcGen(chat.mdcSeed, tempId).unwrap().mdc!!
+            val ciphertext = Base64.decode(task.ciphertextBase64, Base64.NO_WRAP)
+            val request = RequestFactory.buildSndRequest(tempId, ciphertext, mdc)
+            val responseResult = makeRequest(request)
+            if(responseResult.isErr()) continue
+            val response = responseResult.unwrap()
+            if(response.code == 200) {
+                val responseBody = response.body
+                val messageId = responseBody!!.string().toUShort()
+                // TODO: check message number and wait for new messages if it is higher than expected
+                responseBody.close()
+                val message = Message.fromSerialized(task.message).unwrap()
+                message.sent = System.currentTimeMillis() / 1000
+                message.id = chat.messages.size.toULong()
+                chat.messages.add(message)
+                ChatManager.updateChat(chat)
+            }
+        }
         return err("not implemented")
     }
 
