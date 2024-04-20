1d132229bd5cf88293ab0ff55ea70e33d40c9f86
Laurenz Werner <werner.la@protonmail.com>
2024-04-20 12:24:21 +0200

    add timestampFromUnix

--- app/src/main/java/dawn/android/LibraryConnector.kt
+++ app/src/main/java/dawn/android/LibraryConnector.kt
@@ -340,6 +340,12 @@ object LibraryConnector {
         return ok(libraryResponse)
     }
 
+    fun mTimestampFromUnix(timestamp: String): Result<Timestamp, String> {
+        val libraryResponse: Timestamp = Json.decodeFromString(timestampFromUnix(timestamp))
+        if(libraryResponse.status != "ok") return err(libraryResponse.status)
+        return ok(libraryResponse)
+    }
+
     fun mGenHandle(init_pubkey_kyber: String, init_pubkey_curve: String, init_pubkey_curve_pfs_2: String, init_pubkey_kyber_for_salt: String, init_pubkey_curve_for_salt: String, name: String, mdc: String): Result<GenHandle, String> {
         val libraryResponse: GenHandle = Json.decodeFromString(genHandle(init_pubkey_kyber, init_pubkey_curve, init_pubkey_curve_pfs_2, init_pubkey_kyber_for_salt, init_pubkey_curve_for_salt, name, mdc))
         if(libraryResponse.status != "ok") return err(libraryResponse.status)
@@ -392,6 +398,7 @@ object LibraryConnector {
     private external fun decryptFile(ciphertext: ByteArray, key: String): String
     private external fun getCurrentTimestamp(): String
     private external fun getAllTimestampsSince(timestamp: String): String
+    private external fun timestampFromUnix(timestamp: String): String
     private external fun genHandle(init_pubkey_kyber: String, init_pubkey_curve: String, init_pubkey_curve_pfs_2: String, init_pubkey_kyber_for_salt: String, init_pubkey_curve_for_salt: String, name: String, mdc: String): String
     private external fun parseHandle(handle: ByteArray): String
     private external fun genInitRequest(remote_pubkey_kyber: String, remote_pubkey_kyber_for_salt: String, remote_pubkey_curve: String, remote_pubkey_curve_pfs_2: String, remote_pubkey_curve_for_salt: String, own_pubkey_sig: String, own_seckey_sig: String, name: String, comment: String, mdc: String): String
